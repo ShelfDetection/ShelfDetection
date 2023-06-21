15b29059e6abcf1ff4806ed5a92cef566cb87d4f
ShelfDetection <137315146+ShelfDetection@users.noreply.github.com>
2023-06-21 09:54:22 -0600

    Create test.py

---
+++ test.py
@@ -0,0 +1,105 @@
+import time
+import sys
+import argparse
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from jetson_inference import detectNet
+from jetson_utils import videoSource, videoOutput, Log
+
+example_shelf = []
+detected_names = []
+objects_out = []
+
+
+iteration = 1
+
+def notify(mes):
+    #The mail addresses and password
+    sender_address = 'YOUR EMAIL HERE'
+    sender_pass = 'YOUR PASSWORD HERE'
+    receiver_address = 'WHO YOU WANT TO SEND THE EMAIL TO'
+    #Setup the MIME
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = 'Restock' 
+    message.attach(MIMEText(mes, 'plain'))
+    #Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+    session.starttls() #enable security
+    session.login(sender_address, sender_pass) #login with mail_id and password
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
+
+# parse the command line
+parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.", 
+                                 formatter_class=argparse.RawTextHelpFormatter, 
+                                 epilog=detectNet.Usage() + videoSource.Usage() + videoOutput.Usage() + Log.Usage())
+
+parser.add_argument("input", type=str, default="", nargs='?', help="URI of the input stream")
+parser.add_argument("output", type=str, default="", nargs='?', help="URI of the output stream")
+parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre-trained model to load (see below for options)")
+parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
+parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use") 
+
+try:
+	args = parser.parse_known_args()[0]
+except:
+	print("")
+	parser.print_help()
+	sys.exit(0)
+
+# create video sources and outputs
+input = videoSource(args.input, argv=sys.argv)
+output = videoOutput(args.output, argv=sys.argv)
+	
+# load the object detection network
+net = detectNet(args.network, sys.argv, args.threshold)
+
+# note: to hard-code the paths to load a model, the following API can be used:
+#
+# net = detectNet(model="model/ssd-mobilenet.onnx", labels="model/labels.txt", 
+#                 input_blob="input_0", output_cvg="scores", output_bbox="boxes", 
+#                 threshold=args.threshold)
+
+# process frames until EOS or the user exits
+while True:
+    # capture the next image
+    img = input.Capture()
+
+    if img is None: # timeout
+        continue  
+        
+    # detect objects in the image (with overlay)
+    detections = net.Detect(img, overlay=args.overlay)
+
+    # print the detections
+    print("detected {:d} objects in image".format(len(detections)))
+    detected_names = []
+    for i in range(len(detections)):
+        detected_names.append(net.GetClassDesc(detections[i].ClassID))
+        if(iteration == 1):
+            example_shelf.append(net.GetClassDesc(detections[i].ClassID))
+    iteration += 1
+    for i in range(len(example_shelf)):
+        if(example_shelf[i] not in detected_names):
+            if(example_shelf[i] not in objects_out):
+                objects_out.append(example_shelf[i])
+                notify(f"You are out of {example_shelf[i]}s")
+    for detection in detections:
+        print(detection)
+
+    # render the image
+    output.Render(img)
+
+    # update the title bar
+    output.SetStatus("{:s} | Network {:.0f} FPS".format(args.network, net.GetNetworkFPS()))
+
+    # print out performance info
+    net.PrintProfilerTimes()
+
+    # exit on input/output EOS
+    if not input.IsStreaming() or not output.IsStreaming():
+        break
